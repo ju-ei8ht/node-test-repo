57136c8b2105cd34b7cced33561b450e29a6bbd5
ju-ei8ht <qkr8125@gmail.com>
2024-04-15 00:47:22 +0900

    feat: add bookmark of getAllWebtoons

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,9 +1,24 @@
+import { WebtoonDTO } from "../dtos/WebtoonDTO";
 import { WebtoonRepository } from "../repositories/WebtoonRepository"
 
 const webtoonRepository = WebtoonRepository.getInstance();
 const resolvers = {
     Query: {
-        getAllWebtoons: async () => await webtoonRepository.findAllWebtoonsWithSequelize()
+        getAllWebtoons: async () => {
+            const data = await webtoonRepository.findAllWebtoonsIncludeBookmarkWithSequelize();
+            return data.map(webtoon => {
+                return new WebtoonDTO(
+                    webtoon.get().id,
+                    webtoon.get().image,
+                    webtoon.get().title,
+                    webtoon.get().author,
+                    webtoon.get().desc,
+                    webtoon.get().bookmark != null ? true : false,
+                    webtoon.get().bookmark.alarm,
+                    webtoon.get().bookmark.latest
+                );
+            });
+        }
     }
 };
 
